96a62a29699d30d1fe90c19028b6b4a9bc510635
Abikayusri <abikayusri96@gmail.com>
2025-10-13 00:20:20 +0700

    First commit

---
+++ composeApp/src/jvmMain/kotlin/abika/sinau/chirp/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package abika.sinau.chirp
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
