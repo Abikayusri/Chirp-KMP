2bad0526a2d57c33c9d000c068a82e4f75b33274
Abikayusri <abikayusri96@gmail.com>
2025-10-19 15:05:40 +0700

    feat: convention plugin of android applications

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -23,3 +23,19 @@ kotlin {
         jvmTarget = JvmTarget.JVM_17
     }
 }
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "abika.sinau.convention.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
